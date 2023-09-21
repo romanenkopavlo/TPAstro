c4ae52fa3d69ff2365336bff43a9a4028d273ce2
pavlo <paihitetty444@gmail.com>
2023-09-21 11:29:08 +0200

    Les changements dernieres

---
+++ src/LesSignes.java
@@ -0,0 +1,18 @@
+import clavier.In;
+
+public class LesSignes {
+    public static void main(String[] args) {
+        int laDate;
+        String leMois;
+
+        System.out.println("Entrer votre date de naissance: ");
+        laDate = In.readInteger();
+        System.out.println("Entre votre mois de naissance: ");
+        leMois = In.readString();
+        leMois = leMois.toLowerCase();
+
+        if (laDate <= 20 && leMois == new String("mars")) {
+            System.out.println("Hello");
+        }
+    }
+}
