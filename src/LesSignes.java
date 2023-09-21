b5e1920048598b00b5b87c34af14be38e6f76333
pavlo <paihitetty444@gmail.com>
2023-09-21 11:46:02 +0200

    Les changements dernieres

--- src/LesSignes.java
+++ src/LesSignes.java
@@ -11,8 +11,6 @@ public class LesSignes {
         leMois = In.readString();
         leMois = leMois.toLowerCase();
 
-
-
         if (laDate <= 20 && leMois == new String("mars")) {
             System.out.println("Hello");
         }
